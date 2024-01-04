e5285d50068410a063cde1f93f0ac2ad2fae0465
premj21 <premjadhav378@gmail.com>
2024-01-04 10:14:11 +0530

    setup backend server with routes

---
+++ backend/models/User.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name."],
+    },
+    email: {
+      type: String,
+      required: [true, "Please enter your email."],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: [true, "Please enter your phone number."],
+      validate: {
+        validator: function (v) {
+          return /^\d{10}$/.test(v);
+        },
+        message: (props) =>
+          `${props.value} is not a valid phone number. Please enter a 10-digit number.`,
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter a password."],
+      minlength: [6, "Password should be at least 6 characters long."],
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    tokenOfChangePassword: {
+      type: String,
+    },
+    OTP: {
+      type: Number,
+      minLength: [4, "OTP must be at least 4 characters long."],
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+UserSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+UserSchema.methods.gentoken = function () {
+  return jwt.sign({ _id: this._id }, process.env.SEC);
+};
+module.exports = mongoose.model("User", UserSchema);
---
+++ backend/server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const dbconnect = require("./config/dbconnect");
+require("dotenv").config({ path: "./config/config.env" });
+const cookieparser = require("cookie-parser");
+const User = require("./routes/User");
+// const Post = require('./routes/Post');
+const cloudinary = require("cloudinary");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const app = express();
+
+// app.use("/", express.static("../client/build"));
+dbconnect();
+app.use(cookieparser());
+app.use(cors());
+app.use(express.json({ limit: "500mb" }));
+app.use(express.urlencoded({ limit: "100mb", extended: true }));
+
+app.use("/api/v1/auth", User);
+// app.use('/api/post',Post);
+const port = process.env.PRT;
+app.listen(port, () => {
+  console.log(`surver is running on port ${port}`);
+});
